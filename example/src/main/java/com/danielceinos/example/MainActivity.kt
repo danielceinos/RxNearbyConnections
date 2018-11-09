f331b412523f6bd90427ec4816e1c2c06b9adb11
Daniel Sanchez <daniel.ceinos@gmail.com>
2018-11-09 12:29:36 +0100

    Added jitpack dependency

--- example/src/main/java/com/danielceinos/example/MainActivity.kt
+++ example/src/main/java/com/danielceinos/example/MainActivity.kt
@@ -2,13 +2,12 @@ package com.danielceinos.example
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.danielceinos.rxnearbyconnections.RxNearbyConnections
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Rx
     }
 }
